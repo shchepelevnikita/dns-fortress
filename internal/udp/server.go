b8572e8b2a1e9734686c0bebada3e8407608607e
Nikita Shchepelev <nikita.shchepelev@7bits.it>
2024-06-20 22:08:32 +0600

    feat: implement dns query forwarding to cloudflare

--- internal/udp/server.go
+++ internal/udp/server.go
@@ -1,8 +1,11 @@
 package udp
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net"
+	"net/http"
 )
 
 func StartUDPServer() {
@@ -11,6 +14,10 @@ func StartUDPServer() {
 		Port: 8080,
 		Zone: "",
 	}
+
+	cloudflareURL := "https://1.1.1.1/dns-query?name="
+
+	client := &http.Client{}
 
 	conn, err := net.ListenUDP("udp", addr)
 
@@ -24,18 +31,45 @@ func StartUDPServer() {
 	for {
 		var buf [512]byte
 
-		_, addr, err := conn.ReadFromUDP(buf[0:])
+		_, _, err := conn.ReadFromUDP(buf[0:])
 
 		if err != nil {
 			fmt.Printf("Something went wrong when trying to read from UDP server: %v\n", err)
 			return
 		}
 
-		_, err = conn.WriteToUDP([]byte("Hello UDP Client\n"), addr)
+		domain := string(bytes.Trim(buf[:], "\x00"))
+
+		req, err := http.NewRequest("GET", cloudflareURL+""+domain, nil)
 
 		if err != nil {
-			fmt.Printf("Something went wrong when trying to send response over UDP server: %v\n", err)
+			fmt.Printf("Something went wrong when trying to create GET request for resolving %s with Cloudflare: %v\n", domain, err)
 			return
 		}
+
+		req.Header.Add("Accept", "application/dns-json")
+
+		res, err := client.Do(req)
+
+		if err != nil {
+			fmt.Printf("Something went wrong when trying to forward domain %s to Cloudflare: %v\n", domain, err)
+			return
+		}
+
+		resBody, err := io.ReadAll(res.Body)
+
+		if err != nil {
+			fmt.Printf("Something went wrong when trying to read response body: %v\n", err)
+			return
+		}
+
+		fmt.Printf("Response body: %s\nResponse code: %d\n", resBody, res.StatusCode)
+
+		//_, err = conn.WriteToUDP([]byte("Hello UDP Client\n"), addr)
+		//
+		//if err != nil {
+		//	fmt.Printf("Something went wrong when trying to send response over UDP server: %v\n", err)
+		//	return
+		//}
 	}
 }
